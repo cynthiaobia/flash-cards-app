76320599c1c1d228e0163409210ac331df7f3efd
Cynthia <cynthia.obia@gmail.com>
2023-09-24 22:56:04 -0500

    added state to flip flash card

--- src/components/FlashCardSet/FlashCard.js
+++ src/components/FlashCardSet/FlashCard.js
@@ -1,17 +1,25 @@
+import { useState } from 'react';
 
-function FlashCard (props) {
-  const {question, answer, isCorrect} = props.flashCard;
+function FlashCard(props) {
+  const { flashCard } = props;
 
-  return(
+  const [showResult, setShowResult] = useState(false);
+
+  // Function to toggle the showResult state
+  const flipCard = () => {
+    setShowResult((prevShowResult) => !prevShowResult);
+  };
+
+  return (
     <div>
-      <h2>{question}</h2>
-      <h3>{answer}</h3>
+      <h2 onClick={flipCard}>
+        {showResult ? flashCard.answer : flashCard.question}
+      </h2>
       <h4>
-        Mark as correct: 
-        {isCorrect ? <b>Yes</b> : <b>No</b>}
+        Mark as correct: {flashCard.isCorrect ? <b>Yes</b> : <b>No</b>}
       </h4>
     </div>
-  )
+  );
 }
 
 export default FlashCard;
